b46eb14cd4923d5c8cbbd13626032940594bda0b
Lovin Nagi <lovinnagi23@gmail.com>
2025-05-05 18:13:01 +0530

    add content in homepage-package.php

--- page-home.php
+++ page-home.php
@@ -23,6 +23,7 @@ get_header(); ?>
         get_template_part('template-parts/homepage', 'itineraries');
         get_template_part('template-parts/homepage', 'activities');
         get_template_part('template-parts/homepage', 'dining');
+        get_template_part('template-parts/homepage', 'package');
 
         // If comments are open or we have at least one comment, load up the comment template.
         if (comments_open() || get_comments_number()) :
---
+++ template-parts/homepage-package.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bali_honey_moon_static
+ */
+
+?>      
+      <!-- ===================== Services & Package Section Start ================== -->
+      <section class="container m-auto">
+      <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <div class="md:col-span-9 md:pr-6">
+		                <h2 id="the-cost-of-a-bali-honeymoon" class="mb-3 mt-8">
+          The Cost of a Bali Honeymoon
+        </h2>
+        <p class="mb-3">
+          A Bali honeymoon can be expensive, especially when frequenting fine
+          dining restaurants, fancy clubs and the best honeymoon suites.
+          However, a Bali honeymoon is possible on almost any budget. No matter
+          what level of luxury you and your partner prefer, you will be
+          guaranteed to get <strong>amazing value for money</strong> in Bali. We
+          said it before, but luxurious hotels and restaurants cost a fraction
+          of what they cost in most other countries in the world.
+        </p>
+        <p class="mb-3">
+          A <strong>reasonable budget</strong> without going overboard would be
+          around 300USD per night, including food, accomodation and activities.
+          This does require some conscious choices. If you would like to
+          experience the best hotels in Bali, you will likely spend more than
+          that on just accomodation. Some hotels, like the Bulgari or Four
+          Seasons, offer their best honeymoon suites at rates well beyond
+          1000USD per night. Beach Clubs and nightlife can get expensive too -
+          often expecting a minimum spend for their best tables or sunbeds.
+          Don’t let that scare you however: you can really have a honeymoon in
+          Bali on any budget. There are couples who manage to get by with less
+          than 100USD per night!
+        </p>
+        <p class="mb-3">Some tips to keep the budget within reason:</p>
+        <ul class="pl-5 list-disc">
+          <li class="pb-3">
+            Ubud, Seminyak, Uluwatu and Canggu are known to be more expensive
+            regions, while Nusa Penida, Amed and Lovina are known to be
+            <strong>cheaper regions</strong>. Combining a luxurious location
+            with a more affordable region might be an option.
+          </li>
+          <li class="pb-3">
+            Fine dining with your partner is amazing. But Bali has amazing
+            <strong>street food</strong> at extremely low prices. Give it a try!
+          </li>
+          <li class="pb-3">
+            Especially in Canggu and Seminyak: the beachfront hotels are
+            expensive. Look a little further <strong>inland</strong>. You can
+            still get the beach experience by visiting a beach club for a day!
+          </li>
+        </ul>
+		</div>
+	</div>
+</section>
+      <!-- ===================== Services & Package Section End ================== -->
