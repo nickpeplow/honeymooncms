99220241b928874c574f7ebb6e35ec0ce8f6e9bc
Lovin Nagi <lovinnagi23@gmail.com>
2025-05-06 09:49:33 +0530

    add faq section

--- page-home.php
+++ page-home.php
@@ -25,6 +25,7 @@ get_header(); ?>
         get_template_part('template-parts/homepage', 'dining');
         get_template_part('template-parts/homepage', 'package');
         get_template_part('template-parts/homepage', 'other');
+        get_template_part('template-parts/homepage', 'faq');
 
         // If comments are open or we have at least one comment, load up the comment template.
         if (comments_open() || get_comments_number()) :
---
+++ template-parts/homepage-faq.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bali_honey_moon_static
+ */
+
+?>
+
+<section class="container m-auto">
+      <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <div class="md:col-span-9 md:pr-6">
+		    <h2 id="frequently-asked-questions" class="mb-3 mt-8">
+			  Frequently Asked Questions
+			</h2>
+			<section class="text-gray-700">
+            <div class="container mx-auto">
+              <div class="flex flex-wrap sm:mx-auto sm:mb-2 -mx-2">
+                <div class="w-full pt-2 lg:pb-2">
+                  <details class="mb-4 border rounded-lg border-color">
+                    <summary
+                      class="font-semibold bg-brown rounded-md py-2 px-4 flex items-center gap-2.5 text-black"
+                    >
+                      <img
+                        src="<?php echo get_template_directory_uri(); ?>/assets/images/questionmark.png"
+                        class=""
+                        style="width: 20px; height: 20px"
+                      />
+                      Is Bali a good Honeymoon destination?
+                    </summary>
+
+                    <span class="p-3 block">
+                      Bali is a great honeymoon destination. One of the world's
+                      most romantic places, that has a lot to offer in nature,
+                      culture and affordable luxury. Beautiful beaches, jungle
+                      and ricefields. Fine dining and amazing nightlife.
+                      Beautiful sunsets and romantic places to explore, discover
+                      them all in our Honeymoon Guide.
+                    </span>
+                  </details>
+                  <details class="mb-4 border rounded-lg border-color">
+                    <summary
+                      class="font-semibold bg-brown rounded-md py-2 px-4 flex items-center gap-2.5 text-black"
+                    >
+                      <img
+                        src="<?php echo get_template_directory_uri(); ?>/assets/images/questionmark.png"
+                        class=""
+                        style="width: 20px; height: 20px"
+                      />
+                      What is the best region in Bali for a Honeymoon?
+                    </summary>
+
+                    <span class="p-3 block">
+                      Seminyak, Uluwatu and Ubud are the most popular honeymoon
+                      destinations in Bali. However the island is very diverse
+                      and has a lot to offer for any couple!
+                    </span>
+                  </details>
+                  <details class="mb-4 border rounded-lg border-color">
+                    <summary
+                      class="font-semibold bg-brown rounded-md py-2 px-4 flex items-center gap-2.5 text-black"
+                    >
+                      <img
+                        src="<?php echo get_template_directory_uri(); ?>/assets/images/questionmark.png"
+                        class=""
+                        style="width: 20px; height: 20px"
+                      />
+                      Is Kuta in Bali a good honeymoon destination.
+                    </summary>
+
+                    <span class="p-3 block">
+                      We don't think so. Kuta does not have very romantic vibes.
+                      This is where you find masses of tourists looking for
+                      cheap liquor. Nearby Seminyak has more upscale vibes.
+                    </span>
+                  </details>
+                </div>
+              </div>
+            </div>
+          </section>
+		</div>
+	</div>
+</section>
