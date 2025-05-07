745c614fcfb40984a3d2fd3fffd3d016fb8a67f9
Lovin Nagi <lovinnagi23@gmail.com>
2025-05-07 16:52:37 +0530

    set dynamic content for homepage-introduction.php

--- template-parts/homepage-introduction.php
+++ template-parts/homepage-introduction.php
@@ -9,71 +9,29 @@
  */
 
 ?>
+
+<?php
+$page_id = get_the_ID(6);
+
+// Get each field value
+$intro_heading  = get_field('intro_heading', $page_id);
+$intro_paragraph = get_field('intro_paragraph', $page_id);
+?>
+
 <!-- ===================== Introduction Start ================== -->
 <section class="container m-auto">
       <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
         <div class="md:col-span-9 md:pr-6">
+        <?php if ($intro_heading): ?>
 		    <h2 id="bali-honeymoon-the-perfect-romantic-destination" class="mb-3">
-            Bali Honeymoon: the perfect romantic destination
+        <?php echo esc_html($intro_heading); ?>
           </h2>
+          <?php endif; ?>
+          <?php if ($intro_paragraph): ?>
           <p class="my-3">
-            <strong
-              >The island of Bali in Indonesia is one of the world’s most
-              romantic places, and a perfect honeymoon destination. It offers
-              spectacular beaches, jungle forests, volcanoes and ricefields to
-              explore with your partner. But there’s more than nature! Bali has
-              areas with amazing nightlife and many high-end
-              <a
-                class="text-tertiary underline font-bold"
-                href="/stay/most-luxurious-hotels-bali/"
-                >luxurious resorts</a
-              >, 5-star hotels and
-              <a
-                class="text-tertiary underline font-bold"
-                href="/stay/private-pool-villas-bali/"
-                >private pool villas</a
-              >.</strong
-            >
+          <?php echo wp_kses_post($intro_paragraph); ?>
           </p>
-          <p>
-            We believe Bali is the perfect honeymoon destination! Bali offers
-            amazing beaches and nature to explore, a unique hospitable culture,
-            delicious food and many
-            <a
-              class="text-tertiary underline font-bold"
-              href="/explore/romantic-activities-bali/"
-              >romantic activities</a
-            >
-            for couples. All of this can be experienced while comfortably
-            staying in luxury accomodation. Bali has an unmatched offering of
-            <strong>affordable luxury hotels</strong> and resorts - at a
-            fraction of the price you would pay anywhere else in the world. It
-            is a diverse island, with popular tourist areas as well as secluded
-            spots for you and your partner to retreat to (check out our
-            suggested
-            <a
-              class="text-tertiary underline font-bold"
-              href="/stay/private-pool-villas-bali/"
-              >private pool villas</a
-            >).
-          </p>
-          <p class="my-3">
-            Looking for a stress-free honeymoon?
-            <strong>Let us help you!</strong> Choose one of our carefully
-            crafted
-            <a
-              class="text-tertiary underline font-bold"
-              href="/services/bali-honeymoon-packages/"
-              >Bali Honeymoon Packages</a
-            >
-            or let us
-            <a
-              class="text-tertiary underline font-bold"
-              href="/services/honeymoon-planning/"
-              >design your custom itinerary</a
-            >
-            ❤️.     
-          </p>
+          <?php endif; ?>
 		</div>
 	</div>
 </section>
