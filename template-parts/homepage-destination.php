9e38892bc070e64e386c08f77b0459856c706969
Lovin Nagi <lovinnagi23@gmail.com>
2025-05-07 16:02:12 +0530

    set dynamic content for homepage-destination.php

--- template-parts/homepage-destination.php
+++ template-parts/homepage-destination.php
@@ -9,141 +9,130 @@
  */
 
 ?>
+
+<?php
+$page_id = get_the_ID(6);
+
+// Group 1
+$highlight_title_1       = get_field('highlight_title_1', $page_id);
+$highlight_description_1 = get_field('highlight_description_1', $page_id);
+$highlight_image_1_1     = get_field('highlight_image_1_1', $page_id);
+$highlight_image_1_2     = get_field('highlight_image_1_2', $page_id);
+$highlight_image_1_3     = get_field('highlight_image_1_3', $page_id);
+
+// Group 2
+$highlight_title_2       = get_field('highlight_title_2', $page_id);
+$highlight_description_2 = get_field('highlight_description_2', $page_id);
+$highlight_image_2_1     = get_field('highlight_image_2_1', $page_id);
+$highlight_image_2_2     = get_field('highlight_image_2_2', $page_id);
+$highlight_image_2_3     = get_field('highlight_image_2_3', $page_id);
+
+
+// Group 3
+$highlight_title_3       = get_field('highlight_title_3', $page_id);
+$highlight_description_3 = get_field('highlight_description_3', $page_id);
+$highlight_image_3_1     = get_field('highlight_image_3_1', $page_id);
+$highlight_image_3_2     = get_field('highlight_image_3_2', $page_id);
+$highlight_image_3_3     = get_field('highlight_image_3_3', $page_id);
+?>
+
 <section class="container m-auto">
       <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
         <div class="md:col-span-9 md:pr-6">
-		        <h3 class="mb-3 mt-8">Tropical Island</h3>
+        <?php if ($highlight_title_1 || $highlight_description_1): ?>
+		        <h3 class="mb-3 mt-8"><?php echo esc_html($highlight_title_1); ?></h3>
           <p>
-            Bali is a tropical island in the Indian Ocean, part of the
-            Indonesian archipelago in South-East Asia. The island’s
-            <strong>beautiful landscapes</strong> are formed by millions of
-            years of volcanic activity, as well as a humid, tropical climate.
-            Rivers have carved majestic valleys through the island. Bordering
-            the wild Indian Ocean on the south and calm Java Sea on the north, a
-            wide variety of beautiful beaches form the border between land and
-            sea.
+          <?php echo wp_kses_post($highlight_description_1); ?>
           </p>
           <div class="grid grid-cols-3 gap-4 my-11">
             <div class="border border-white border-dashed rounded-lg">
-              <img
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/mount_agung_small.webp"
-                class="rounded-lg w-full"
-              />
+            <?php if ($highlight_image_1_1): ?>
+              <img src="<?php echo esc_url($highlight_image_1_1['url']); ?>" alt="<?php echo esc_attr($highlight_image_1_1['alt']); ?>" class="rounded-lg w-full" />
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">
                 Tropical Forest with Agung Volcano in the background.
               </p>
             </div>
             <div class="border border-white border-dashed rounded-lg">
-              <img
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/rice_terraces_small.webp"
-                class="rounded-lg w-full"
-              />
+            <?php if ($highlight_image_1_2): ?>
+              <img src="<?php echo esc_url($highlight_image_1_2['url']); ?>" alt="<?php echo esc_attr($highlight_image_1_2['alt']); ?>" class="rounded-lg w-full" />
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">
                 Rice Terraces stacked vertically against a hill near Ubud.
               </p>
             </div>
             <div class="border border-white border-dashed rounded-lg">
-              <img
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/uluwatu_cliffs_small.webp"
-                class="rounded-lg w-full"
-              />
+            <?php if ($highlight_image_1_3): ?>
+              <img src="<?php echo esc_url($highlight_image_1_3['url']); ?>" alt="<?php echo esc_attr($highlight_image_1_3['alt']); ?>" class="rounded-lg w-full" />
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">
                 Cliffs with a white-sand beach near Uluwatu.
               </p>
             </div>
           </div>
-
-          <h3 class="mb-3 mt-8">Tropical Island</h3>
+          <?php endif; ?>
+          <?php if ($highlight_title_2 || $highlight_description_2): ?>
+          <h3 class="mb-3 mt-8"><?php echo esc_html($highlight_title_2); ?></h3>
           <p>
-            Bali is a tropical island in the Indian Ocean, part of the
-            Indonesian archipelago in South-East Asia. The island’s
-            <strong>beautiful landscapes</strong> are formed by millions of
-            years of volcanic activity, as well as a humid, tropical climate.
-            Rivers have carved majestic valleys through the island. Bordering
-            the wild Indian Ocean on the south and calm Java Sea on the north, a
-            wide variety of beautiful beaches form the border between land and
-            sea.
+          <?php echo wp_kses_post($highlight_description_2); ?>
           </p>
           <div class="grid grid-cols-3 gap-4 my-11">
             <div class="border border-white border-dashed rounded-lg">
-              <img
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/ulan_danu_temple_small.webp"
-                class="rounded-lg w-full"
-              />
+            <?php if ($highlight_image_2_1): ?>
+              <img src="<?php echo esc_url($highlight_image_2_1['url']); ?>" alt="<?php echo esc_attr($highlight_image_2_1['alt']); ?>" class="rounded-lg w-full"/>
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">
                 Ulan Danu Temple on Beratan lake in central Bali.
               </p>
             </div>
             <div class="border border-white border-dashed rounded-lg">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/images/kecak_small.webp" class="rounded-lg w-full" />
+            <?php if ($highlight_image_2_2): ?>
+              <img src="<?php echo esc_url($highlight_image_2_2['url']); ?>" alt="<?php echo esc_attr($highlight_image_2_2['alt']); ?>" class="rounded-lg w-full" />
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">
                 Traditional Kecak Firedance near Uluwatu Temple.
               </p>
             </div>
             <div class="border border-white border-dashed rounded-lg">
-              <img
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/lempuyang_temple_small.webp"
-                class="rounded-lg w-full"
-              />
+            <?php if ($highlight_image_2_3): ?>
+              <img src="<?php echo esc_url($highlight_image_2_3['url']); ?>" alt="<?php echo esc_attr($highlight_image_2_3['alt']); ?>" class="rounded-lg w-full"/>
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">
                 Lempuyang Temple overlooking the Agung volcano.
               </p>
             </div>
           </div>
-          <h3 class="mb-3 mt-8">Luxury & Romance</h3>
+          <?php endif; ?>
+          <?php if ($highlight_title_3 || $highlight_description_3): ?>
+          <h3 class="mb-3 mt-8"><?php echo esc_html($highlight_title_3); ?></h3>
           <p>
-            In Bali, your options as a newlywed couple are almost endless. The
-            island is known for its
-            <a
-              class="text-tertiary underline font-bold"
-              href="/stay/most-luxurious-hotels-bali/"
-              >luxurious hotels</a
-            >, fancy beachclubs and
-            <a
-              class="text-tertiary underline font-bold"
-              href="/explore/best-fine-dining-in-bali/"
-              >fine dining</a
-            >. It is definitely possible to splurge on fancy accomodation for
-            you and your partner. The most spectacular honeymoon suites do come
-            at a cost. However, in general, Bali is very affordable, and the
-            island is great on any budget. Lovely
-            <a
-              class="text-tertiary underline font-bold"
-              href="/explore/best-bali-beach-clubs/"
-              >beach clubs</a
-            >
-            are scattered around the island, especially the south. Fine dining
-            restaurants can be found in the Ubud, Uluwatu and Seminyak regions.
-            Some restaurants offer romantic private dining, in a secluded spot
-            surrounded with candle lights!
+          <?php echo wp_kses_post($highlight_description_3); ?>
           </p>
           <div class="grid grid-cols-3 gap-4 my-11">
             <div class="border border-white border-dashed rounded-lg">
-              <img
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/viceroy_ubud_small.webp"
-                class="rounded-lg w-full"
-              />
+            <?php if ($highlight_image_3_1): ?>
+              <img src="<?php echo esc_url($highlight_image_3_1['url']); ?>" alt="<?php echo esc_attr($highlight_image_3_1['alt']); ?>" class="rounded-lg w-full"/>
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">
                 Luxurious Viceroy hotel in Ubud.
               </p>
             </div>
             <div class="border border-white border-dashed rounded-lg">
-              <img
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/la_brisa_small.webp"
-                class="rounded-lg w-full"
-              />
+            <?php if ($highlight_image_3_2): ?>
+              <img src="<?php echo esc_url($highlight_image_3_2['url']); ?>" alt="<?php echo esc_attr($highlight_image_3_2['alt']); ?>" class="rounded-lg w-full"/>
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">
                 La Brisa Beachclub in Canggu.
               </p>
             </div>
             <div class="border border-white border-dashed rounded-lg">
-              <img
-                src="<?php echo get_template_directory_uri(); ?>/assets/images/karma_beach_small.webp"
-                class="rounded-lg w-full"
-              />
+            <?php if ($highlight_image_3_3): ?>
+              <img src="<?php echo esc_url($highlight_image_3_3['url']); ?>" alt="<?php echo esc_attr($highlight_image_3_3['alt']); ?>" class="rounded-lg w-full"/>
+            <?php endif; ?>
               <p class="text-sm text-center pt-2">Karma Beach near Uluwatu.</p>
             </div>
           </div>
+          <?php endif; ?>
 		</div>
 	</div>
 </section>
