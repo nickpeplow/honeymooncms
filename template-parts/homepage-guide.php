cad6ed8bc0ac94412da1645afc6b77138f10b159
Lovin Nagi <lovinnagi23@gmail.com>
2025-05-07 16:31:06 +0530

    set dynamic content for homepage-guide.php

--- template-parts/homepage-guide.php
+++ template-parts/homepage-guide.php
@@ -9,31 +9,36 @@
  */
 
 ?>
+
+<?php
+$page_id = get_the_ID(6);
+
+// Get each field value
+$map_title         = get_field('map_title', $page_id);
+$small_description = get_field('small_description', $page_id);
+$map_image         = get_field('map_image', $page_id);
+$map_description   = get_field('map_description', $page_id);
+?>
+
 <section class="container m-auto">
       <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
         <div class="md:col-span-9 md:pr-6">
+          <?php if ($map_title): ?>
 		        <h2 id="bali-regions-and-map " class="mb-3 mt-8">
-            Bali regions and map
+            <?php echo esc_html($map_title); ?>
           </h2>
+          <?php endif; ?>
+          <?php if ($small_description): ?>
           <p>
-            The island of Bali is larger than most people expect, and has many
-            distinct regions. It takes around 3 hours to drive from north to
-            south or from west to east. Most people
-            <strong>combine a few regions</strong> during their honeymoon in
-            Bali to get a full taste of what the island has to offer. Check out
-            our 8 ready-made
-            <a
-              class="text-tertiary underline font-bold"
-              href="#best-bali-honeymoon-itineraries"
-              >Bali Honeymoon Itineraries</a
-            >
-            for inspiration!
+            <?php echo wp_kses_post($small_description); ?>
           </p>
+          <?php endif; ?>
+          <?php if ($map_image): ?>
           <div
             class="border border-dotted p-3 my-5 border-color rounded-lg border-dashed"
           >
             <img
-              src="<?php echo get_template_directory_uri(); ?>/assets/images/balihoneymoonmap.webp"
+              src="<?php echo esc_url($map_image['url']); ?>"
               class="rounded-lg w-full"
             />
             <p class="text-center d-block text-sm text-center pt-2">
@@ -41,99 +46,12 @@
               Click to enlarge.
             </p>
           </div>
+          <?php endif; ?>
+          <?php if ($map_description): ?>
           <p class="mb-3">
-            <strong>South Bali</strong> is the most developed and touristy
-            region on the island. This is where you find the most luxury hotels
-            and nightlife. The south is also famous for its beaches and surf.
-            The airport is conveniently located near the popular tourist spots
-            in South bali. Our guide will help you find the right spots in South
-            Bali for a romantic honeymoon. We love
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/uluwatu-honeymoon/"
-              >Uluwatu</a
-            >,
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/seminyak-honeymoon/"
-              >Seminyak</a
-            >
-            and
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/canggu-honeymoon/"
-              >Canggu</a
-            >. We recommend against spending your honeymoon in Kuta - it’s
-            overrun by mass tourism and there are many more romantic places in
-            Bali. Another option in South Bali is
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/nusa-dua-honeymoon/"
-              >Nusa Dua</a
-            >, an area known for its large luxurious resorts right in front of a
-            beautiful beach.
+          <?php echo wp_kses_post($map_description); ?>
           </p>
-          <p>
-            <strong>North Bali</strong> is a different world, away from the
-            crowds: spectacular nature with dense jungle, 100-meter high
-            waterfalls, volcanoes, crater lakes, calm beaches, hot springs and
-            more. North Bali is where you will still find very traditional
-            Balinese life. Our articles on
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/amed-honeymoon/"
-              >Amed</a
-            >
-            and
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/lovina-honeymoon/"
-              >Lovina</a
-            >
-            will help you and your partner to find some romantic secluded spots!
-          </p>
-          <p class="mt-3">
-            Between the North and the South, in Central Bali, you will find the
-            spectacular jungle town of <strong>Ubud</strong>. Made famous by the
-            Movie Eat Pray Love, Ubud is <strong>our favorite</strong> honeymoon
-            destination on the island. Check out our
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/ubud-honeymoon/"
-              >Ubud Honeymoon Guide</a
-            >
-            to learn why!
-          </p>
-          <p class="mt-3">
-            Some more remote <strong>off the beaten path</strong> destinations
-            in Central and East Bali are the
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/munduk-honeymoon/"
-              >Munduk Region</a
-            >
-            on the slope of a volcanic crater and the serene
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/sidemen-honeymoon/"
-              >Sidemen</a
-            >
-            village surrounded by rice fields and mountains. Or consider staying
-            at the beautiful calm beach town of
-            <a
-              class="text-tertiary underline font-bold"
-              href="/regions/candidasa-honeymoon/"
-              >Candidasa</a
-            >. While these are more uncommon destinations for a honeymoon, both
-            have a luxury boutique hotel we would recommend, so check our
-            guides!
-          </p>
-          <p class="mt-3 mb-7">
-            We have published <strong>honeymoon guides</strong> to each of the
-            different regions. Use the filter buttons below to find the perfect
-            destination for you and your partner!
-          </p>
-    
+          <?php endif; ?>
 		</div>
 	</div>
 </section>
