b0d007b4af8201b874f084a20228c7f7a0a0f6d4
Lovin Nagi <lovinnagi23@gmail.com>
2025-05-08 10:58:38 +0530

    set dynamic content for homepage-other.php

--- template-parts/homepage-other.php
+++ template-parts/homepage-other.php
@@ -10,155 +10,84 @@
 
 ?>
 
+<?php
+$page_id = get_the_ID(6);
+
+// Get each field value
+$section_title  = get_field('section_title', $page_id);
+$section_intro = get_field('section_intro', $page_id);
+$gallery_images_1 = get_field('gallery_images_1', $page_id);
+$gallery_images_2 = get_field('gallery_images_2', $page_id);
+$gallery_images_3 = get_field('gallery_images_3', $page_id);
+$small_dscription = get_field('small_dscription', $page_id);
+$explore_map_image = get_field('explore_map_image', $page_id);
+$full_description = get_field('full_description', $page_id);
+?>
+
 <section class="container m-auto">
       <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
         <div class="md:col-span-9 md:pr-6">
-		                <h2 id="indonesia-beyond-bali" class="mb-3 mt-8">
-          Indonesia beyond Bali
-        </h2>
-        <p class="mb-3">
-          As we lived in Bali for many years, we had the opportunity to explore
-          many corners of Indonesia. Olivia is originally from
-          <strong>North Sumatra</strong>, a region famous for orangutans and one
-          the largest volcanic lake in the world. Unfortunately, this region is
-          a bit far from Bali (it requires 2 flights to get there!). However,
-          there are many great regions closer to Bali that we love to recommend
-          for a honeymoon in Indonesia.
-        </p>
+        <?php if ($section_title): ?>
+          <h2 id="indonesia-beyond-bali" class="mb-3 mt-8">
+            <?php echo esc_html($section_title); ?>
+          </h2>
+          <?php endif; ?>
+          <?php if ($section_intro): ?>
+          <p class="mb-3">
+            <?php echo wp_kses_post($section_intro); ?>
+          </p>
+          <?php endif; ?>
         <div class="grid grid-cols-3 gap-4 my-11">
           <div class="border border-white border-dashed rounded-lg">
+          <?php if ($gallery_images_1): ?>
             <img
-              src="<?php echo get_template_directory_uri(); ?>/assets/images/viceroy_ubud_small.webp"
+              src="<?php echo esc_url($gallery_images_1['url']); ?>" alt="<?php echo esc_attr($gallery_images_1['alt']); ?>"
               class="rounded-lg w-full"
             />
+            <?php endif; ?>
             <p class="text-sm text-center pt-2">
-              Luxurious Viceroy hotel in Ubud.
+              Komodo & Flores.
             </p>
           </div>
           <div class="border border-white border-dashed rounded-lg">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/la_brisa_small.webp" class="rounded-lg w-full" />
+          <?php if ($gallery_images_2): ?>
+            <img src="<?php echo esc_url($gallery_images_2['url']); ?>" alt="<?php echo esc_attr($gallery_images_2['alt']); ?>" class="rounded-lg w-full" />
+            <?php endif; ?>
             <p class="text-sm text-center pt-2">
-              La Brisa Beachclub in Canggu.
+              Raja Ampat.
             </p>
           </div>
           <div class="border border-white border-dashed rounded-lg">
+          <?php if ($gallery_images_3): ?>
             <img
-              src="<?php echo get_template_directory_uri(); ?>/assets/images/karma_beach_small.webp"
+              src="<?php echo esc_url($gallery_images_3['url']); ?>" alt="<?php echo esc_attr($gallery_images_3['alt']); ?>"
               class="rounded-lg w-full"
             />
-            <p class="text-sm text-center pt-2">Karma Beach near Uluwatu.</p>
+            <?php endif; ?>
+            <p class="text-sm text-center pt-2">Mount Bromo.</p>
           </div>
         </div>
-        <p class="mb-3">
-          Be aware that Bali is very luxurious and easy to travel compared to
-          the rest of Indonesia. If you plan to enjoy fine dining and splurge on
-          5-star
-          <a class="text-tertiary underline font-bold" href="#"
-            >luxurious hotels</a
-          >, we would recommend staying in Bali.
-        </p>
-        <p class="mb-3">
-          But if you are an adventurous couple… Indonesia is a beautiful large
-          and diverse archipelago with over 17.000 islands. Some great
-          destinations can be reached <strong>by ferry</strong> from Bali, while
-          other destinations will require a <strong>domestic flight</strong> or
-          even multiple flights. Below we list our favorite regions adventurous
-          couples should consider when visiting Indonesia.
-        </p>
-
-        <div
-          class="border border-dotted p-3 my-5 border-color rounded-lg border-dashed"
-        >
-          <img
-            src="<?php echo get_template_directory_uri(); ?>/assets/images/visit_indonesia_beyond_bali.webp"
-            class="rounded-lg w-full"
-          />
+        <?php if ($small_dscription): ?>
+          <p class="mb-3">
+            <?php echo wp_kses_post($small_dscription); ?>
+          </p>
+        <?php endif; ?>
+        <div class="border border-dotted p-3 my-5 border-color rounded-lg border-dashed">
+          <?php if ($explore_map_image): ?>
+            <img
+              src="<?php echo esc_url($explore_map_image['url']); ?>" alt="<?php echo esc_attr($explore_map_image['alt']); ?>"
+              class="rounded-lg w-full"
+            />
+          <?php endif; ?>
           <p class="text-center d-block text-sm text-center pt-2">
             Our favorite places to visit in Indonesia.
           </p>
         </div>
+        <?php if ($full_description): ?>
         <ul class="pl-5 list-disc">
-          <li class="pb-3">
-            Visit the neighbouring island of
-            <strong
-              ><a class="text-tertiary underline font-bold" href="#"
-                >Lombok</a
-              ></strong
-            >
-            for surf (Kuta), hiking (Rinjani) or party and diving (<a
-              class="text-tertiary underline font-bold"
-              href="#"
-              >Gili Islands</a
-            >). Take the ferry from Padangbai or take a 30 minute flight from
-            Bali to Praia.
-          </li>
-          <li class="pb-3">
-            Between Bali and Lombok you will find the three
-            <strong>Nusa Islands</strong>, of which
-            <a class="text-tertiary underline font-bold" href="#"
-              >Nusa Penida</a
-            >
-            is the largest and we believe the most interesting. It offers iconic
-            cliffs and beaches to explore. Nusa Lebongan and Nusa Ceningan
-            island are getting more popular too, but are much smaller and don’t
-            offer as stunning landscapes.
-          </li>
-          <li class="pb-3">
-            Climb the <strong>Iljen and Bromo</strong> volcanoes in East Java.
-            Reachable by short ferry from Gilimanuk in west Bali, or by direct
-            plane from Bali to Surabaya.
-          </li>
-          <li class="pb-3">
-            Explore the cultural heart of Java and visit the world’s largest
-            temple Borobodur in <strong>Yogjakarta</strong>. Direct flights from
-            Bali to Yogyakarta are available.
-          </li>
-          <li class="pb-3">
-            Say hi to komodo dragons and dive with manta rays around
-            <strong>Flores and the Komodo Islands</strong>. Take a direct flight
-            to Labuan Bajo on Flores Island. From there you can board a
-            multi-day boat trip to the Komodo Islands, or stay overnight and
-            join any of the day trips to Komodo.
-          </li>
-          <li class="pb-3">
-            See orang-utans in Bukit Lawang and discover Lake Toba, the largest
-            volcanic lake in the world, in <strong>North Sumatra</strong>. We
-            love this region (Olivia grew up here!). Not many direct flights
-            from Bali to Medan are available, most options will require a
-            transit in Jakarta. Medan is easy to reach with direct flights from
-            Singapore and Kuala Lumpur too.
-          </li>
-          <li class="pb-3">
-            Explore the underwater world in the world-famous
-            <strong>Raja Ampat</strong> in West Papua. This is said to be one of
-            the world’s best dive sights - but it is really hard to get here! It
-            will involve multiple flights and a ferry.
-          </li>
-          <li class="pb-3">
-            Go off the beaten path and discover the beauty of
-            <strong>Sumba Island</strong> - great for surf and pristine beaches.
-            Direct flights to Tambolaka are available. Oddly, the world’s most
-            luxurious resort, the Nihi Sumba, can be found on this island.
-          </li>
-          <li class="pb-3">
-            Indonesia’s capital <strong>Jakarta</strong> is not known as a
-            honeymoon destination. However, many people enter the country here,
-            and there is a lot of history to see, food to enjoy and some luxury
-            hotels to stay at. In addition, it could be a starting point for
-            discovering Java island by train.
-          </li>
-          <li class="pb-3">
-            <strong>Bandung</strong> is one of the nicer cities in Indonesia. It
-            is very green, nestled against the West Java mountains. Worth
-            visiting if you plan to travel overland from Java to Bali.
-          </li>
-          <li class="pb-3">
-            The island of <strong>Wakatobi</strong> is a luxurious dive
-            destination near Sulawesi. Guests who stay at the Wakatobi Resort
-            can hop on the complimentary private jet(!) from Bali to fly to the
-            island.
-          </li>
+          <?php echo wp_kses_post($full_description); ?>
         </ul>
+        <?php endif; ?>
 		</div>
 	</div>
 </section>
