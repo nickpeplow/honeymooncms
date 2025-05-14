6a94521b796b784caae21ba9dc9427a79d5f9f4f
Lovin Nagi <lovinnagi23@gmail.com>
2025-05-14 18:06:22 +0530

    small changes in these file

--- template-parts/homepage-regions.php
+++ template-parts/homepage-regions.php
@@ -60,7 +60,7 @@
               $experience_terms = wp_get_post_terms(get_the_ID(), 'destination_experience_type');
               ?>
               <div class="flex">
-                <div class="basis-1/3">
+                <div class="basis-[100%] sm:basis-1/3">
                   <figure class="min-[280px]:w-1/1 p-2 relative">
                     <?php if (has_post_thumbnail()) : ?>
                       <?php the_post_thumbnail('medium', ['class' => 'w-full h-[280px] min-[280px]:h-full object-cover rounded-lg']); ?>
@@ -74,7 +74,7 @@
                   </figure>
                 </div>
 
-                <div class="basis-2/3">
+                <div class="basis-[100%] sm:basis-2/3">
                   <div class="flex flex-col justify-center p-3 pl-3">
                     <div class="flex justify-between mb-1">
                       <header>
