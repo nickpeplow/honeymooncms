8e991b9fdd6a5899f9219f1cb184c1c17a96499a
Lovin Nagi <lovinnagi23@gmail.com>
2025-05-26 12:06:49 +0530

    create page template for beachclub and her template-parts

---
+++ template-parts/BeachClub/explore-beachclub.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bali_honey_moon_static
+ */
+
+?>
+
+<!-- Club 1 -->
+
+<h2 id="finns-beach-club" class="mb-0 mt-6">1. Finns Beach Club (Canggu)</h2>
+<span class="italic text-gray-500">Berawa near Canggu</span>
+<div class="grid grid-cols-2 gap-4">
+    <div class="text-hover">
+        <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/7976-1731839550-small.webp" class="rounded-lg w-full" />
+        </a>
+    </div>
+    <div class="text-hover">
+        <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/4175-1731841427-small.webp" class="rounded-lg w-full" />
+        </a>
+    </div>
+</div>
+<p>
+    Finns claims to be the "world's best beach club". It definitely is the best beach club in Bali if you are looking to celebrate and party. Finns, strategically located on Berawa beach between <a class="text-tertiary underline font-bold" href="/regions/canggu-honeymoon/">Canggu</a> and <a class="text-tertiary underline font-bold" href="/regions/seminyak-honeymoon/">Seminyak</a>, is one of the most well known beach clubs in Bali. You can join a beach party at Finns 365 days a year.<br><br>
+
+    Finns is great for those who like to party: DJs are playing even during the day, they always have performers like dancers and musicians playing. It feels like a party well before sunset. It also means it might not be the best fit if you are looking for a calm romantic experience.<br><br>
+
+    Beyond party and performances, we like to simply watch the surf from one of the infinity pools at Finns. The food is very good too and fairly priced for a beach club. You can order multiple cuisines from the comfort of your day-bed, including Japanese, Mexican, Italian, Chinese, Indian and Indonesian.<br><br>
+
+    Due to its popularity, Finns expanded a lot lately. While they still have the original beach club with bamboo bar and ocean-facing pool, there now are new Finns VIP and Finns Beach Party areas. These are more spacious and feel more premium.<br><br>
+
+    Finns is popular, and can sometimes get very crowded. Definitely book your sunbeds in advance. They require a minimum spend amount. The Beach Party area is the most crowded and lively, which is part of the charm of Finns, but the VIP Area might be a good choice if you like a little more personal space. 
+</p>
+<a class="read_more font-bold" href="https://finnsbeachclub.com/" target="_blank">FINNS Beach Club Website</a>
+
+<!-- Club 2 -->
+
+<h2 id="finns-beach-club" class="mb-0 mt-6">2. KU DE TA Beachclub</h2>
+<span class="italic text-gray-500">Seminyak</span>
+<div class="grid grid-cols-2 gap-4">
+    <div class="text-hover">
+        <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/one-small (2).webp" class="rounded-lg w-full" />
+        </a>
+    </div>
+    <div class="text-hover">
+        <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/7202-1742623204-small.webp" class="rounded-lg w-full" />
+        </a>
+    </div>
+</div>
+<p>Ku De Ta is one of Bali’s most iconic beach clubs, offering a luxurious beachfront experience in the heart of Seminyak. With stunning ocean views and a prime sunset-watching location, it’s a must-visit destination for travelers looking to relax in style. Guests can book a spacious daybed for 2-3 people in advance through the website, and the credit is redeemable for food and drinks. For those who prefer a more flexible option, the restaurant and bar are open with no minimum spending required.</p>
+                
+<p>Unlike some beach clubs, Ku De Ta is known for its exceptional dining. Enjoy gourmet small plates like octopus or tuna tartare while taking in the sunset, or indulge in wood-fired steak for a full-course meal. 
+For a more elevated dining experience, visit Saltlick on the second floor, where premium dishes and refined flavors take center stage.</p>
+                
+<p>Whether you're looking for a relaxing day by the beach, handcrafted cocktails, or world-class dining, Ku De Ta Seminyak delivers an unforgettable experience.</p>
+                
+<a class="read_more font-bold" href="https://www.kudeta.com/" target="_blank">Official Website</a>
+
+<!-- Club 3 -->
+
+<h2 id="finns-beach-club" class="mb-0 mt-6">3. Jungle Fish Infinity Pool & Bar</h2>
+<span class="italic text-gray-500">15m drive north of Ubud</span>
+<div class="grid grid-cols-2 gap-4">
+    <div class="text-hover">
+        <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/8150-1742620340-small.webp" class="rounded-lg w-full" />
+        </a>
+    </div>
+    <div class="text-hover">
+        <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/8461-1742620358-small.webp" class="rounded-lg w-full" />
+        </a>
+    </div>
+</div>                
+<p>The Jungle Fish Infinity Pool Bar is a great place to get away from the hustle and bustle of Ubud. Jungle Fish is excellent to relax and enjoy with your partner. Take in the beautiful jungle scenery. Enjoy some food and cocktails from the pool bar. Take a refreshing dip in the calm and clear pool water. This hidden gem surrounded by a forest is perfect for couples!</p>
+<p>Jungle Fish is part of the Chapung Se Bali resort. Reserve a day at a Jungle Fish in advance to ensure a peaceful experience, as access is limited.</p>
+                
+<a class="read_more font-bold" href="https://www.chapung.com/ubud/restaurants/jungle-fish-pool-bar/" target="_blank">Visit Website</a>
---
+++ template-parts/BeachClub/explore-introduction.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package bali_honey_moon_static
+ */
+
+?>
+
+<p class="my-3">
+    <p><strong>Bali boasts some of the best Beach Clubs in the world. Most of them can be found facing the Indian Ocean just north of Bali’s airport, near the towns of <a class="text-tertiary underline font-bold" href="/regions/seminyak-honeymoon/">Seminyak</a> and <a class="text-tertiary underline font-bold" href="/regions/canggu-honeymoon/">Canggu</a>. Another region with fancy ocean-front Day Clubs is <a class="text-tertiary underline font-bold" href="/regions/uluwatu-honeymoon/">Uluwatu</a>, however often they are on a cliff, so while you can view the ocean, its hard to reach the actual beach!</strong></p>
+
+    <p>We believe a visit to a Beach Club is a <em>must-do</em> during any <a class="text-tertiary underline font-bold" href="/">Bali honeymoon</a> or vacation as a couple. We have been living in Bali for over 6 years and have visited many, sometimes together as a couple and sometimes with friends. Below are our favorites.</p>
+
+    <p>Couples visiting Bali might also be interested in our list of best <a class="text-tertiary underline font-bold" href="/explore/romantic-wellness-in-bali/">wellness</a>, <a class="text-tertiary underline font-bold" href="/explore/best-fine-dining-in-bali/">fine dining</a>, <a class="text-tertiary underline font-bold" href="/stay/most-luxurious-hotels-bali/">most luxury hotels</a> or <a class="text-tertiary underline font-bold" href="/stay/private-pool-villas-bali/">private pool villas</a> in Bali.</p>
+</p>
